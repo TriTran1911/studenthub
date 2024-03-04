430107efc35c19d4df451c302a209017c61193e8
TriTran1911 <trandaoxuantricr123@gmail.com>
2024-03-05 06:21:21 +0700

    dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/homepage.dart';
-
+import '../components/project.dart';
 void main() {
+
+  Project.addProject('Project 1', ProjectDuration.oneToThreeMonths, 'Description 1', 'Working');
+  Project.addProject('Project 2', ProjectDuration.threeToSixMonths, 'Description 2', 'Achieved');
+  Project.addProject('Project 3', ProjectDuration.oneToThreeMonths, 'Description 3', 'Working');
+  Project.addProject('Project 4', ProjectDuration.threeToSixMonths, 'Description 4', 'Achieved');
+  Project.addProject('Project 5', ProjectDuration.oneToThreeMonths, 'Description 5', 'Working');
+  Project.addProject('Project 6', ProjectDuration.threeToSixMonths, 'Description 6', 'Achieved');
+
   runApp(MyApp());
 }
 
@@ -9,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Navigation Demo',
+      title: 'StudentHub',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
