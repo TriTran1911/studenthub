825100285ea984aedede12407684ff68b6261cf4
ldkhoa10 <ldkhoa20@vp.fitus.edu.vn>
2024-03-07 14:15:10 +0700

    Student profile input

--- lib/main.dart
+++ lib/main.dart
@@ -34,11 +34,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
       ),
-<<<<<<< Updated upstream
-      home: ProfileInputScreen(),
-=======
       home: SwitchAccountScreen(),
->>>>>>> Stashed changes
     );
   }
 }
