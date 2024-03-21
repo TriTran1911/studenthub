67ec8a3f996e1bc6467358721985a520afc3e1bd
TriTran1911 <trandaoxuantricr123@gmail.com>
2024-03-21 14:40:18 +0700

    alerts page

--- lib/screens/HomePage/alerts/alerts.dart
+++ lib/screens/HomePage/alerts/alerts.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import '/components/appbar.dart';
 
-class AlertsPage extends StatelessWidget {
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Alerts Page'),
+    return MaterialApp(
+      title: 'Project App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: AlertsPage(),
+    );
+  }
+}
+
+class AlertsPage extends StatefulWidget {
+
+  AlertsPage();
+
+  @override
+  _AlertsPageState createState() => _AlertsPageState();
+}
+
+class _AlertsPageState extends State<AlertsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Scaffold(
+      body: Center(
+        child: Text('Alerts'),
+      ),
     );
   }
 }
