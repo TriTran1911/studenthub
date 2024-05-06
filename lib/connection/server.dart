8a3dd6f32c1f1f41e6ba99972c74788e47c47aaf
ldkhoa10 <ldkhoa20@vp.fitus.edu.vn>
2024-05-06 20:57:08 +0700

    Check

--- lib/connection/server.dart
+++ lib/connection/server.dart
@@ -4,18 +4,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '/components/modelController.dart';
 
 const String url_b = "https://api.studenthub.dev";
+
 class Connection {
   static Future<dynamic> postRequest(String url, dynamic body) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
+    print(url_b + url);
     var response = await http.post(
       Uri.parse(url_b + url),
       headers: <String, String>{
+        'accept': '*/*', // 'application/json
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token',
+        // 'Authorization': 'Bearer $token',
       },
       body: json.encode(body),
     );
+    print(response.body ?? 'null');
     if (response.statusCode == 201 || response.statusCode == 200) {
       print("Connect server successful");
       return response.body;
@@ -25,7 +29,8 @@ class Connection {
     }
   }
 
-  static Future<dynamic> getRequest(String api, Map<String, dynamic> json) async {
+  static Future<dynamic> getRequest(
+      String api, Map<String, dynamic> json) async {
     var url = Uri.parse(url_b + api);
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('token');
@@ -64,7 +69,8 @@ class Connection {
     }
   }
 
-  static Future<void> putLanguage(int studentId, List<Language> languageList) async {
+  static Future<void> putLanguage(
+      int studentId, List<Language> languageList) async {
     print('Put Language');
     String url = '/api/language/updateByStudentId/$studentId';
     try {
