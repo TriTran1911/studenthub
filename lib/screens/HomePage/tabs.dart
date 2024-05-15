75ff20fe7d57f5c8f7160c037b56311a645cfbdd
Nguyen Minh Triet <minhtrietcm02@gmail.com>
2024-05-16 03:06:53 +0700

    fix theme

--- lib/screens/HomePage/tabs.dart
+++ lib/screens/HomePage/tabs.dart
@@ -1,5 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:studenthub/components/theme_provider.dart';
 import 'dashBoard/SdashBoard.dart';
 import 'dashBoard/CDashBoard.dart';
 import 'projects/projects.dart';
@@ -12,7 +14,7 @@ import '/components/appbar.dart';
 class TabsPage extends StatefulWidget {
   final int index;
 
-  const TabsPage({super.key, required this.index});
+  const TabsPage({Key? key, required this.index}) : super(key: key);
 
   @override
   _TabsPageState createState() => _TabsPageState();
@@ -27,47 +29,60 @@ class _TabsPageState extends State<TabsPage> {
     _selectedIndex = widget.index;
   }
 
-  final List<TabInfo> _tabs = [
-    TabInfo(
-        page: ProjectsPage(role: modelController.user.roles[0]),
-        label: tr('tab_text1'),
-        icon: Icons.list_alt),
-    TabInfo(
-        page: modelController.user.roles[0] == 1
-            ? const CompanyDashboardPage()
-            : const StudentDashboardPage(),
-        label: tr('tab_text2'),
-        icon: Icons.dashboard),
-    TabInfo(page: MessagePage(), label: tr('tab_text3'), icon: Icons.chat),
-    TabInfo(
-        page: AlertsPage(), label: tr('tab_text4'), icon: Icons.notifications),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final List<TabInfo> _tabs = [
+      TabInfo(
+          page: ProjectsPage(role: modelController.user.roles[0]),
+          label: tr('tab_text1'),
+          icon: Icons.list_alt),
+      TabInfo(
+          page: modelController.user.roles[0] == 1
+              ? const CompanyDashboardPage()
+              : const StudentDashboardPage(),
+          label: tr('tab_text2'),
+          icon: Icons.dashboard),
+      TabInfo(page: MessagePage(), label: tr('tab_text3'), icon: Icons.chat),
+      TabInfo(
+          page: AlertsPage(),
+          label: tr('tab_text4'),
+          icon: Icons.notifications),
+    ];
+
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
     return Scaffold(
       appBar: const CustomAppBar(backWard: false),
       body: _tabs[_selectedIndex].page,
-      bottomNavigationBar: BottomNavigationBar(
-        items: _tabs.map((tab) => _buildBottomNavigationBarItem(tab)).toList(),
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped,
+      bottomNavigationBar: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return BottomNavigationBar(
+            items: _tabs
+                .map((tab) => _buildBottomNavigationBarItem(tab, themeProvider))
+                .toList(),
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: Colors.grey,
+            backgroundColor: themeProvider.getTheme().scaffoldBackgroundColor,
+            onTap: _onItemTapped,
+          );
+        },
       ),
     );
   }
 
-  BottomNavigationBarItem _buildBottomNavigationBarItem(TabInfo tab) {
+  BottomNavigationBarItem _buildBottomNavigationBarItem(
+      TabInfo tab, ThemeProvider themeProvider) {
     return BottomNavigationBarItem(
       icon: Icon(tab.icon),
       label: tab.label,
+      backgroundColor: themeProvider.getTheme().scaffoldBackgroundColor,
     );
   }
 }
+
+
